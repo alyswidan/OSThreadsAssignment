c2357311a407d1e84946bb1f8680266df04c03d7
alyswidan <swidan12340@gmail.com>
2017-11-21 14:39:38 +0200

    definitions pushed to a header file

--- Multiply.c
+++ Multiply.c
@@ -7,35 +7,7 @@
 #include <time.h>
 #include <pthread.h>
 #include <sys/time.h>
-
-typedef struct MatMultParams{
-    double** A;
-    double** B;
-    double** C;
-    int A_r,A_c,B_r,B_c;
-}MatMultParams_t;
-
-typedef struct DotParams{
-    double* a;
-    double* b;
-    int sz_a,sz_b;
-    double *result;
-}DotParams_t;
-
-typedef void *matmult(void*);
-void initDotParams(DotParams_t*,double* a,double* b,int sz_a,int sz_b,double *result);
-void initMatMultParams(MatMultParams_t*,double** A, double** B, double** C, int A_r,int A_c,int B_r,int B_c);
-double benchmark(matmult,void*);
-double get_time();
-void *nonThreadedMatMult(void*);
-void *ThreadedMatMultPerElement(void*);
-void *ThreadMatMultPerRow(void*);
-double** initMatrix(int,int);
-void* dot(void *params);
-double **transpose(double**, int,int);
-void initRandomMat(double**,int,int,int);
-void printMat(double**,int,int,char*);
-void  printVector(double*,int);
+#include "Multiply.h"
 double **A,**B,**C;
 int A_r = 5,A_c=5,B_r=5,B_c=5,C_r,C_c;
 const int limit = 50;
